9f86b6bc2090d01a0e388862a0f119bb3751a03b
Roman Gudov <gudov.p2@mail.ru>
2023-11-10 20:39:37 +0300

    Change branch

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -8,28 +8,33 @@ public class Radio {
         radioChannel = newRadioChannel;
     }
 
-    public void setMaxChannel() {
+    public void setMaxChannel(int newSetMaxChannel) {
+        radioChannel = newSetMaxChannel;
         radioChannel = 9;
     }
 
-    public void setNextChannel() {
-        if (radioChannel > 9) ;
+    public void setNextChannel(int newSetNextChannel) {
+        radioChannel = newSetNextChannel;
+        if (radioChannel < 9)
         {
             radioChannel = radioChannel + 1;
-            radioChannel = 0;
+            }else {
+            radioChannel=0;
         }
-    }
+        }
 
-    public void setPreviousChannel() {
-        if (radioChannel <= 0) {
-            radioChannel = radioChannel - 1;
+
+    public void setPreviousChannel(int newSetPreviousChannel) {
+        radioChannel = newSetPreviousChannel;
+        if (radioChannel > 0) {
+            radioChannel = radioChannel - 1;}else {
             radioChannel = 9;
         }
     }
 
     public void setIncreaseVolume(int newSetIncreaseVolume) {
         currentVolume = newSetIncreaseVolume;
-        if (currentVolume <= 100) {
+        if (currentVolume < 100) ;{
             currentVolume = currentVolume + 1;
         }
         if (currentVolume > 100) {
@@ -39,7 +44,7 @@ public class Radio {
 
     public void setReductionVolume(int newSetReductionVolume) {
         currentVolume = newSetReductionVolume;
-        if (currentVolume >= 0) {
+        if (currentVolume > 0) ;{
             currentVolume = currentVolume - 1;
         }
         if (currentVolume < 0) {
--- src/test/java/org/example/RadioTest.java
+++ src/test/java/org/example/RadioTest.java
@@ -21,7 +21,7 @@ public class RadioTest {
     @Test
     public void setMaxChanel() {
         Radio radio = new Radio();
-        radio.setMaxChannel();
+        radio.setMaxChannel(9);
 
         int expected = 9;
         int actual = radio.radioChannel;
@@ -31,12 +31,22 @@ public class RadioTest {
     }
 
     @Test
-    public void setNextChanel() {
+    public void setNextChannel() {
         Radio radio = new Radio();
-        int radioChanel = 10;
-        radio.setNextChannel();
+        radio.setNextChannel(9);
 
         int expected = 0;
+        int actual = radio.radioChannel;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNextChannel2(){
+        Radio radio = new Radio();
+        radio.setNextChannel(5);
+
+        int expected = 6;
         int actual = radio.radioChannel;
 
         Assertions.assertEquals(expected, actual);
@@ -45,8 +55,7 @@ public class RadioTest {
     @Test
     public void setPreviousChannel() {
         Radio radio = new Radio();
-        int radioChannel=-1;
-        radio.setPreviousChannel();
+        radio.setPreviousChannel(0);
 
         int expected = 9;
         int actual = radio.radioChannel;
@@ -55,20 +64,51 @@ public class RadioTest {
     }
 
     @Test
+    public void setPreviousChannel2() {
+        Radio radio = new Radio();
+        radio.setPreviousChannel(5);
+
+        int expected = 4;
+        int actual = radio.radioChannel;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     public void setIncreaseVolume() {
         Radio radio = new Radio();
-        radio.setIncreaseVolume(101);
+        radio.setIncreaseVolume(40);
+        int expected = 41;
+        int actual = radio.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setIncreaseVolume2() {
+        Radio radio = new Radio();
+        radio.setIncreaseVolume(100);
         int expected = 0;
         int actual = radio.currentVolume;
 
         Assertions.assertEquals(expected, actual);
     }
 
+
     @Test
     public void setReductionVolume() {
         Radio radio = new Radio();
-        radio.setReductionVolume(-1);
+        radio.setReductionVolume(0);
         int expected = 100;
+        int actual = radio.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setReductionVolume2() {
+        Radio radio = new Radio();
+        radio.setReductionVolume(22);
+        int expected = 21;
         int actual = radio.currentVolume;
 
         Assertions.assertEquals(expected, actual);
